1db33e1675bcc568846f629cd9486e578e1e37b0
Packingdustry <alois.mc@hotmail.com>
2024-05-15 11:11:53 +0200

    ajoute le test ChargeurMagasin

--- test/ChargeurMagasinTest.java
+++ test/ChargeurMagasinTest.java
@@ -1,11 +1,30 @@
 package test;
 
+import XML.ChargeurMagasin;
+import donnees.Magasin;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ChargeurMagasinTest {
 
     @Test
-    void chargerMagasin() {
+    void chargerMagasin() throws FileNotFoundException {
+        ChargeurMagasin cm = new ChargeurMagasin("fichiers");
+        Magasin m = cm.chargerMagasin();
+        assertEquals(m.getNombreCds(), 12, "Il devrait y avoir 12 CDs. ");
+    }
+
+    @Test
+    void chargerMagasinException() {
+        ChargeurMagasin cm = new ChargeurMagasin("abc");
+
+        FileNotFoundException exception = assertThrows (
+                FileNotFoundException.class,
+                () -> {Magasin m = cm.chargerMagasin();}
+        );
     }
 }
