90bb3e8435663c64444e2e74fefabf0ecc3920eb
Packingdustry <alois.mc@hotmail.com>
2024-05-24 17:08:50 +0200

    ajoute les tests

--- test/MagasinTest.java
+++ test/MagasinTest.java
@@ -5,6 +5,7 @@ import donnees.*;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,9 +15,7 @@ class MagasinTest {
     void trierTitre() throws FileNotFoundException {
         ChargeurMagasin cm = new ChargeurMagasin("fichiers");
         Magasin m = cm.chargerMagasin();
-        System.out.println(m);
         m.trierTitre();
-        System.out.println(m);
         ComparateurAlbum comp = new ComparateurAlbum();
         assertTrue(comp.etreAvant(m.getCd(0), m.getCd(11)), "Le 1er CD devrait être avant le dernier. ");
     }
@@ -26,7 +25,28 @@ class MagasinTest {
         ChargeurMagasin cm = new ChargeurMagasin("fichiers");
         Magasin m = cm.chargerMagasin();
         m.trierArtiste();
+        System.out.println(m);
         ComparateurArtiste comp = new ComparateurArtiste();
         assertTrue(comp.etreAvant(m.getCd(0), m.getCd(11)), "Le 1er CD devrait être avant le dernier. ");
+    }
+
+    @Test
+    void chercherArtiste() throws FileNotFoundException {
+        ChargeurMagasin cm = new ChargeurMagasin("fichiers");
+        Magasin m = cm.chargerMagasin();
+        ArrayList<CD> cdZebda = m.chercherArtiste("Zebda");
+        assertEquals(1, cdZebda.size(), "La liste devrait contenir 1 CD. ");
+        assertEquals("Zebda", cdZebda.get(0).getNomArtiste(), "L'artiste devrait être \"Céline Dion\". ");
+        assertEquals("Essence ordinaire", cdZebda.get(0).getNomCD(), "Le titre devrait être \"Essence ordinaire\". ");
+    }
+
+    @Test
+    void chercherTitre() throws FileNotFoundException {
+        ChargeurMagasin cm = new ChargeurMagasin("fichiers");
+        Magasin m = cm.chargerMagasin();
+        ArrayList<CD> cdZebda = m.chercherTitre("Essence ordinaire");
+        assertEquals(1, cdZebda.size(), "La liste devrait contenir 1 CD. ");
+        assertEquals("Zebda", cdZebda.get(0).getNomArtiste(), "L'artiste devrait être \"Céline Dion\". ");
+        assertEquals("Essence ordinaire", cdZebda.get(0).getNomCD(), "Le titre devrait être \"Essence ordinaire\". ");
     }
 }
